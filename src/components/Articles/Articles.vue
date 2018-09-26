59f9881817cf59e5065f1ca386ae7d3c88b7e405
Bartosz Łaniewski <bartosz@laniewski.me>
2018-09-26 19:55:15 +0200

    Add basic news mockup

---
+++ src/components/Articles/Articles.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-row>
+    <b-col lg="4" sm="6" xs="12" class="mb-4"><Article /></b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4"><Article /></b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4"><Article /></b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4"><Article /></b-col>
+    <b-col lg="4" sm="6" xs="12" class="mb-4"><Article /></b-col>
+  </b-row>
+</template>
+
+<script>
+import Article from "./Article";
+
+export default {
+  name: "Articles",
+  components: {
+    Article
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/screens/News.vue
+++ src/components/screens/News.vue
@@ -1,11 +1,35 @@
 <template>
   <b-container>
-    <p>News here</p>
+    <b-jumbotron bg-variant="transparent" text-variant="white">
+      <template slot="header">
+        <span class="d-none d-md-block">Crypto news</span>
+      </template>
+
+      <template slot="lead">
+        <p class="jumbotron-lead">
+          Latest articles from the world of cryptocurrencies. Read what is
+          happening with <em>Bitcoin</em>, <em>Ethereum</em> and other altcoins.
+        </p>
+      </template>
+    </b-jumbotron>
+
+    <Articles />
   </b-container>
 </template>
 
 <script>
+import Articles from "../Articles/Articles.vue";
+
 export default {
-  name: 'NewsScreen'
+  name: 'NewsScreen',
+  components: {
+    Articles
+  }
 }
 </script>
+
+<style scoped>
+.jumbotron-lead {
+  max-width: 600px;
+}
+</style>
