d778fa851fb3cbffa4e972acfd4d8959a0e47ddd
Bartosz Łaniewski <bartosz@laniewski.me>
2018-10-03 17:39:34 +0200

    Update footer background gradient

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="footer fixed-bottom w-100 px-3 py-3 text-right">
-    <p class="text-muted my-0">Powered by <b-link class="text-light" href="https://min-api.cryptocompare.com/">CryptoCurrency API</b-link>.</p>
+  <div class="footer fixed-bottom w-100">
+    <p class="text-muted my-0 mx-0">Powered by <b-link class="text-light" href="https://min-api.cryptocompare.com/">CryptoCurrency API</b-link>.</p>
   </div>
 </template>
 
@@ -12,7 +12,20 @@ export default {
 
 <style scoped>
 .footer {
-  background-image: linear-gradient(transparent 5%, #323639 100%);
+  background-image: linear-gradient(transparent 0, #323639 95%);
   font-size: 0.85em;
+
+  padding-top: 1.65rem;
+  padding-bottom: 0.35rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  text-align: right;
+}
+
+@media screen and (max-width: 1300px) {
+  .footer {
+    text-align: center;
+  }
 }
 </style>
