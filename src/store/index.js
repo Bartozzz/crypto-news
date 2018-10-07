6f6703544bc53d9517a7e2ad215592ee6e10d3dd
Bartosz Łaniewski <bartosz@laniewski.me>
2018-10-07 14:30:18 +0200

    Implement persistent state (closes #3)

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,27 @@
+import * as R from "ramda";
 import Vue from "vue";
 import Vuex from "vuex";
 import createLogger from "vuex/dist/logger";
+import createPersistedState from "vuex-persistedstate";
 
 import coins from "./modules/coins";
 import news from "./modules/news";
 import trends from "./modules/trends";
 
+Vue.use(Vuex);
+
 // Disable logs & strict mode in production:
 const debug = process.env.NODE_ENV !== "production";
 
-Vue.use(Vuex);
+const allPlugins = [createPersistedState()];
+const devPlugins = [createLogger()];
+const prodPlugins = [];
 
 export default new Vuex.Store({
   strict: debug,
-  plugins: debug ? [createLogger()] : [],
+  plugins: debug
+    ? R.concat(allPlugins, devPlugins)
+    : R.concat(allPlugins, prodPlugins),
 
   modules: {
     coins,
