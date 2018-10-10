f613e2d76ff317f6603a7819effbeff9a4454bbe
Bartosz Łaniewski <bartosz@laniewski.me>
2018-10-10 19:23:13 +0200

    Update ESLint & Prettier to integrate with vue files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
   root: true,
+
+  extends: [
+    "plugin:vue/essential",
+    "plugin:prettier/recommended",
+    "@vue/prettier"
+  ],
+
+  plugins: ["vue"],
+
   env: {
+    browser: true,
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 2017,
+    sourceType: "module"
+  },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
   }
 };
