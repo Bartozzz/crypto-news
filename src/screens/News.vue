37741c63f07a6bb88f0fe41470115e75f3a5b8ca
Bartosz Łaniewski <bartosz@laniewski.me>
2018-10-11 13:16:06 +0200

    Cleanup News.vue

--- src/screens/News.vue
+++ src/screens/News.vue
@@ -1,15 +1,13 @@
 <template>
   <b-container>
-    <b-jumbotron bg-variant="transparent" text-variant="white">
+    <b-jumbotron bg-variant="dark" text-variant="white">
       <template slot="header">
         <span class="d-none d-md-block">Crypto news</span>
       </template>
 
       <template slot="lead">
-        <p class="jumbotron-lead">
-          Latest articles from the world of cryptocurrencies. Read what is
-          happening with <em>Bitcoin</em>, <em>Ethereum</em> and other altcoins.
-        </p>
+        Latest articles from the world of cryptocurrencies. Read what is
+        happening with <em>Bitcoin</em>, <em>Ethereum</em> and other altcoins.
       </template>
     </b-jumbotron>
 
@@ -22,50 +20,49 @@ import Articles from "../components/Articles/Articles.vue";
 
 export default {
   name: "NewsScreen",
+
   components: {
     Articles
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .jumbotron {
   position: relative;
-}
 
-.jumbotron::before {
-  position: absolute;
-  left: 15px;
-  top: 0px;
+  &::before {
+    position: absolute;
+    left: 15px;
+    top: 0px;
 
-  background-image: url("/images/map.png");
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: center left;
+    background-image: url("/images/map.png");
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center left;
 
-  width: 450px;
-  height: 300px;
+    width: 450px;
+    height: 300px;
 
-  content: "";
-}
+    content: "";
 
-.jumbotron-lead {
-  max-width: 600px;
-}
+    @media screen and (max-width: 480px) {
+      left: 0;
 
-@media screen and (max-width: 820px) {
-  .jumbotron .display-3,
-  .jumbotron .lead {
-    text-align: center;
+      width: 100%;
+      height: 200px;
+    }
   }
-}
 
-@media screen and (max-width: 480px) {
-  .jumbotron::before {
-    left: 0;
+  .display-3,
+  .lead {
+    max-width: 600px;
 
-    width: 100%;
-    height: 200px;
+    @media screen and (max-width: 820px) {
+      margin: 0 auto;
+
+      text-align: center;
+    }
   }
 }
 </style>
